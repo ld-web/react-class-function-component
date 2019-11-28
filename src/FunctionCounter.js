6ce2bfedc7de0a836a965f378b9840d7b967ae9b
Lucas Delobelle <lucas@ld-web.net>
2019-11-28 13:37:52 +0100

    Mise en forme

--- src/FunctionCounter.js
+++ src/FunctionCounter.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const FunctionCounter = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setMyCounter] = useState(0);
 
   useEffect(() => {
     console.log("Valeur du compteur : ", counter);
@@ -9,14 +9,23 @@ const FunctionCounter = () => {
 
   return (
     <div>
-        <h2>Compteur avec composant sous forme de fonction :</h2>
-        <p>Compteur : {counter}</p>
-        <p>
-          <input type="button" onClick={() => setCounter(counter + 1)} value="Incrémenter" style={{marginRight: '1em'}}/>
-          <input type="button" onClick={() => setCounter(counter - 1)} value="Décrémenter"/>
-        </p>
-      </div>
-  )
-}
+      <h2>Compteur avec composant sous forme de fonction :</h2>
+      <p>Compteur : {counter}</p>
+      <p>
+        <input
+          type="button"
+          onClick={() => setMyCounter(counter + 1)}
+          value="Incrémenter"
+          style={{ marginRight: "1em" }}
+        />
+        <input
+          type="button"
+          onClick={() => setMyCounter(counter - 1)}
+          value="Décrémenter"
+        />
+      </p>
+    </div>
+  );
+};
 
 export default FunctionCounter;
